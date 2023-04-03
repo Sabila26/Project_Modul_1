0b304325f75c2b1428ea3c6ae2cccfd00a641594
Lanaaa08 <gusmaulana2117@gmail.com>
2023-04-03 18:55:59 +0700

    komentar

---
+++ komentas.js
@@ -0,0 +1,13 @@
+// ini menampilkan output
+console.log("Selamat Datang di maulana_2117"); // ini nama instagramnya
+console.log("Nama Saya adalah Maulana Irsyad"); // ini menampilkan nama saya
+console.log("Alamat Jurang Gebog Kudus"); // ini menampilkan alamat saya
+console.log("08 Juli 2003 tanggal lahir saya"); // ini menampilkan tangal lahir saya
+
+/**
+ * console.log('ini)
+ * kkjckuehuiqe
+ * kuhdecudeh
+ * jkabwd
+ * 
+ */
