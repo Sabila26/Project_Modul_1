e6dd8d51b12ed7be49114e6884cbad6249745810
Lanaaa08 <127176583+Lanaaa08@users.noreply.github.com>
2023-05-09 08:18:06 +0700

    Add files via upload

---
+++ SwitchCase.js
@@ -0,0 +1,28 @@
+console.log("latihan Switch Case");
+let bahasa = "Jawa";
+    let greeting = null;
+
+    switch (bahasa) {
+        case "Indonesia":
+        greeting = "Selamat Pagi";
+        break;
+        case "Jawa":
+        greeting = "Sugeng Enjing";
+        break;
+        case "English":
+        greeting = "Good Morning";
+        break;
+        case "Turkey":
+        greeting = "Gunaiden";
+        break;
+        case "Sunda":
+        greeting = "Wilujeng Enjing";
+        break;
+        case "Arab":
+        greeting = "Shobahul Kkoir";
+        break;
+        case "Belanda":
+        greeting = "Goedemorgen";
+        default:
+        greeting = "P Inpo...!";
+    }
